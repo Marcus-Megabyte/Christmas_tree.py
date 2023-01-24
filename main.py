15bb1c9d5e6a89daa05dcfd0b252cc486cdb8776
Marques_practice.py <marquesgatley11@gmail.com>
2023-01-24 12:49:59 -0600

    practice_run

---
+++ main.py
@@ -0,0 +1,35 @@
+# importing turtle package
+import turtle
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor('Black')
+# set turtle shape
+turtle.shape("turtle")
+# set the colormode
+turtle.colormode(255)
+# use forward by 100 pixel
+# default pen color is black
+turtle.forward(100)
+# change the pencolor
+# pencolor is red
+turtle.pencolor("red")
+# use forward by 100 pixel
+# then 90 degrees right
+turtle.right(100)
+turtle.forward(80)
+# change the pencolor
+# pencolor is blue
+turtle.pencolor((153, 50, 204))
+# use forward by 100 pixel
+# then 90 degrees right
+turtle.right(90)
+turtle.forward(100)
+# change the pencolor
+# pencolor is green
+turtle.pencolor(255, 255, 0)
+# use forward by 100 pixel
+# then 90 degrees right
+turtle.right(90)
+turtle.forward(100)
+# This code is contributed
+# by Marques Gatley .
